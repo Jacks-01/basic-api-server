7b1c979e53605a312fc0b34e5f7ef7cdd091dae7
Jack Stubblefield <Jack011601@gmail.com>
2022-09-25 15:44:40 -0500

    put request working

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -5,12 +5,42 @@ const { CatModel } = require('../models/');
 
 const router = express.Router();
 
+//* getting all cats
+router.get('/cats', async (req, res, next) => {
+  console.log(`getting all cats...`);
+  const cats = await CatModel.findAll();
+  res.status(200).send(cats);
+  next();
+});
 
+//* get a single cat
+router.get('/cats/:id', async (req, res, next) => {
+  console.log(`getting one cat`);
+  let id = req.params.id;
+
+  const cat = await CatModel.findAll({ where: { id } });
+
+  res.status(200).send(cat);
+  next();
+});
+
+//* creating a new cat
 router.post('/cats', async (req, res, send) => {
   console.log('req body', req.body);
-  
+
   const newCat = await CatModel.create(req.body);
   res.status(200).send(newCat);
 });
-  
+
+//* updating a cat
+router.put('/cats/:id', async (req, res, send) => {
+  let id = req.params.id;
+  console.log(`PUT request for a cat with id: ${id}, req.body: ${req.body}`);
+
+  const cat = await CatModel.update(req.body, { where: { id } });
+
+  const updatedCat = await CatModel.findAll({ where: { id } });
+  res.status(200).send(updatedCat);
+});
+
 module.exports = router;
---
+++ src/routes/hats.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const HatModel = require('../models/');
+const router = require('./cats');
+
+
+const Router = express.Router;
+
+
+router.get('/hats', (req, res, next) => {
+
+});
