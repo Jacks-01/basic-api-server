72b6bbb2cd61bea9add7959bf3e29d58f3cbee80
Jack Stubblefield <Jack011601@gmail.com>
2022-09-25 16:32:05 -0500

    added error handling middleware in server and routes

--- src/routes/cats.js
+++ src/routes/cats.js
@@ -2,50 +2,67 @@
 
 const express = require('express');
 const { CatModel } = require('../models/');
-
 const router = express.Router();
+const errorHandler = require('../error-handlers/500');
 
 //* getting all cats
 router.get('/cats', async (req, res, next) => {
-  console.log(`getting all cats...`);
-  const cats = await CatModel.findAll();
-  res.status(200).send(cats);
-  next();
+  try {
+    console.log(`getting all cats...`);
+    const cats = await CatModel.findAll();
+    res.status(200).send(cats);
+    next();
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
 
 //* get a single cat
 router.get('/cats/:id', async (req, res, next) => {
-  console.log(`getting one cat`);
-  let id = req.params.id;
-
-  const cat = await CatModel.findAll({ where: { id } });
-
-  res.status(200).send(cat);
-  next();
+  try {
+    console.log(`getting one cat`);
+    let id = req.params.id;
+    const cat = await CatModel.findAll({ where: { id } });
+    res.status(200).send(cat);
+    next();
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
 
 //* creating a new cat
 router.post('/cats', async (req, res, send) => {
-  console.log('req body', req.body);
-
-  const newCat = await CatModel.create(req.body);
-  res.status(200).send(newCat);
+  try {
+    console.log('req body', req.body);
+    const newCat = await CatModel.create(req.body);
+    res.status(200).send(newCat);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
 
 //* updating a cat
 router.put('/cats/:id', async (req, res, send) => {
-  let id = req.params.id;
-  await CatModel.update(req.body, { where: { id } });
+  try {
+    let id = req.params.id;
+    await CatModel.update(req.body, { where: { id } });
 
-  const updatedCat = await CatModel.findAll({ where: { id } });
-  res.status(200).send(updatedCat);
+    const updatedCat = await CatModel.findAll({ where: { id } });
+    res.status(200).send(updatedCat);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
 
 //* deleting a cat
 router.delete('/cats/:id', async (req, res, send) => {
-  const id = req.params.id;
-  await CatModel.destroy({ where: { id } });
-  const deletedCat = await CatModel.findAll({ where: { id } });
-  res.status(200).send(deletedCat);
+  try {
+    const id = req.params.id;
+    await CatModel.destroy({ where: { id } });
+    const deletedCat = await CatModel.findAll({ where: { id } });
+    res.status(200).send(deletedCat);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 'use strict';
 const express = require('express');
-const { db, CatModel, HatModel} = require('./models/');
-
-
+const { db, CatModel, HatModel } = require('./models/');
 const PORT = process.env.PORT || 3001;
+const catRouter = require('./routes/cats');
+
+//* import  middleware and error handlers
+const logger = require('./middleware/logger');
+const notFound = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
+
 const app = express();
 app.use(express.json());
-
-//import  middleware and error handlers
-const catRouter = require('./routes/cats');
-const notFound = require('./error-handlers/404');
-const serverError = require('./error-handlers/500');
-
+app.use(logger);
 
 app.use(catRouter);
+
+app.use('*', notFound);
+app.use(errorHandler);
 
 function start() {
   app.listen(PORT, () => {
